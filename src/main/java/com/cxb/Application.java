6c4e21a034f4ba05104584a80481f082c6b56b60
ChenXb <810464826@qq.com>
2018-07-09 14:17:44 +0800

    测试代码  哈哈

---
+++ src/main/java/com/cxb/Application.java
@@ -0,0 +1,10 @@
+package com.cxb;
+
+/**
+ * Created by 81046 on 2018-07-09
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+}
